495dd9af1d7f1101fe7499e439f10367642ddbf8
ibhindal <hindali@cardiff.ac.uk>
2022-11-18 09:13:52 +0000

    start of main program

---
+++ Main Program.py
@@ -0,0 +1,16 @@
+#Main Program
+
+
+
+import os
+import numpy as np
+import meshio
+import areaFun
+import B_function
+import GaussQuad
+import JacobianMat
+import shapeFun
+import shapefunDeri
+import strainDisp2D
+
+#############################################
