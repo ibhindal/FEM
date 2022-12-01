6a7060e5f2c9e5d0578801ed2accf9aec12ef683
ibhindal <hindali@cardiff.ac.uk>
2022-12-01 20:14:01 +0000

    sparce matrix doesnt dot

--- strainDisp2D.py
+++ strainDisp2D.py
@@ -18,8 +18,7 @@ def strainDisp2D(SF,nodeCoor,Jacob) :
     I = Jacob['invJ'] 
     r1 = np.c_[dsfdx,np.zeros(nne)].flatten() 
     r2 = np.c_[dsfde,np.zeros(nne)].flatten() 
-    R = np.dot(I, (np.c_[r1, r2].T) )
-    #R = I.dot(np.c_[r1, r2].T)
+    R = I.dot(np.c_[r1, r2].T)
 
 
 
@@ -27,8 +26,7 @@ def strainDisp2D(SF,nodeCoor,Jacob) :
     dudy = R[1,:] 
     r1 = np.c_[np.zeros(nne),dsfdx].flatten()
     r2 = np.c_[np.zeros(nne),dsfde].flatten() 
-    #R = I.dot(np.c_[r1, r2].T) 
-    R = np.dot(I, (np.c_[r1, r2].T) )
+    R = I.dot(np.c_[r1, r2].T) 
     dvdx = R[0,:] 
     dvdy = R[1,:] 
     B = np.c_[dudx, dvdy, dudy+dvdx].T 
