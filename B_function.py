9ecf53792966dba8d61127f221294eff023eeabc
ibhindal <hindali@cardiff.ac.uk>
2022-12-03 13:06:09 +0000

    update

--- B_function.py
+++ B_function.py
@@ -7,14 +7,16 @@ def B_function(N, dndx, dnde ,nodeXY):
    """
    b function is the strain displacement matrix requuires xi, eta and nodal coords
    Inputs :
-
+        N      : 
+        dndx   :
+        dnde   :
+        nodeXY :
    Returns:
-    B : the strain diplacement matrix 
+        B : the strain diplacement matrix 
    """
     #N, dndx, dnde = shapefunDeri.shapefunDeri(xi, eta) 
- 
 
-    Jacob, SF = {}, {} 
+    Jacob, SF = {}, {} #Jacobi
     SF['sf'], SF['dndx'], SF['dnde'] = N, dndx, dnde 
     Jacob['J'], Jacob['detJ'], Jacob['invJ'] = JacobianMat.ajacob(SF['dndx'], 
                                                                   SF['dnde'], nodeXY) 
