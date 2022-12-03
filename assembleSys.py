1b1e2507403f8eb44aad55c4eff6d506f2a2e30b
ibhindal <hindali@cardiff.ac.uk>
2022-12-03 10:54:11 +0000

    update

--- assembleSys.py
+++ assembleSys.py
@@ -1,28 +1,31 @@
 from numpy import *
 import scipy as sp
-
 from scipy import sparse
 
-
 def assembleSys(K,Kel,con_el) :
-
-    dofpe = Kel.shape[0]
-    npe = con_el.shape[1]     # nodes per element
-    dofpn = dofpe/npe         # dof per node
-    ndof = K.shape[0]
+    """
     
+    Inputs:
+        k:
+        Kel:
+        con_el:
+    Outputs:
+        k:
+    """
+    dofpe     = Kel.shape[0]
+    npe       = con_el.shape[1]     # nodes per element
+    dofpn     = dofpe/npe           # dof per node
+    ndof      = K.shape[0]
     Kelsparse = sp.sparse.csr_matrix(Kel)
+    nelem     = con_el.shape[0]                # number of elements
 
-    nelem = con_el.shape[0]                # number of elements
     for ii in range(nelem) :
-        ix = con_el[ii]
+        ix   = con_el[ii]
         Rind = c_[dofpn*ix, dofpn*ix+1].flatten()
         Cind = arange(dofpe)
         indV = ones(dofpe)
         Emat = sp.sparse.csr_matrix((indV, (Rind,Cind)), shape=(ndof,dofpe))
-
-        K = K + Emat.dot(Kelsparse).dot(Emat.T)
-
+        K    = K + Emat.dot(Kelsparse).dot(Emat.T)
 
     return K
 
