c1ecb9cdd6fa75d4689e079add8add391346e7a8
ibhindal <hindali@cardiff.ac.uk>
2022-12-03 12:46:39 +0000

    update

--- JacobianMat.py
+++ JacobianMat.py
@@ -8,13 +8,14 @@ def ajacob(dndx,dnde,nodecoor) :
     """ 
     calculates Jacobian at the local coordinate point (xi,eta) 
     Inputs : 
-    dndx : vector derivatives of shape functions w.r.t xi (1 by nne) 
-    dnde : vector derivatives of shape functions w.r.t eta (1 by nne) 
-    nodecoor : node coordinate matrix (nne X 2) 
+        dndx : vector derivatives of shape functions w.r.t xi (1 by nne) 
+        dnde : vector derivatives of shape functions w.r.t eta (1 by nne) 
+        nodecoor : node coordinate matrix (nne X 2) 
     
     Returns : 
-    ai : the Jacobian matrix (2 by 2) 
-    detj : the determinant of the Jacobian matrix
+        ai   : the Jacobian matrix (2 by 2) 
+        detj : the determinant of the Jacobian matrix
+        I : 
     """ 
     ai = np.zeros([2,2]) 
     ai[0,0] = np.dot(dndx,nodecoor[:,0]) #sum of dndx with xi 
@@ -22,19 +23,13 @@ def ajacob(dndx,dnde,nodecoor) :
     ai[1,0] = np.dot(dnde,nodecoor[:,0]) #sum of dnde with xi 
     ai[1,1] = np.dot(dnde,nodecoor[:,1]) #sum of dnde with yi 
     
-    # determinant of the Jacobian 
-    detj = ai[0,0]*ai[1,1]- ai[1,0]*ai[0,1] 
-    
-    # calculate the inverse of the jacobian 
-    I= np.zeros
+    detj = ai[0,0]*ai[1,1]- ai[1,0]*ai[0,1] # determinant of the Jacobian 
+     
+    I = np.zeros    # calculate the inverse of the jacobian
     
     if detj == 0 : 
         print("Singular matrix !!!") 
     else : 
-        #print "Jacobian for element ", elemc, "is : ", ai 
-        # #print "determinant of Jacobian is :", detj 
-         
         I = np.linalg.inv(ai) 
-        
         
     return ai, detj, I
