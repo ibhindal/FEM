66520338fd12fcf03526f8ee586b2b19746b1383
ibhindal <hindali@cardiff.ac.uk>
2022-12-08 15:57:00 +0000

    abishek changes

--- assembleSys.py
+++ assembleSys.py
@@ -13,19 +13,33 @@ def assembleSys(K,Kel,con_el) :
         k:
     """
     dofpe     = Kel.shape[0]
-    npe       = con_el.shape[1]     # nodes per element
+    npe       = con_el.shape[0]     # nodes per element
     dofpn     = dofpe/npe           # dof per node
     ndof      = K.shape[0]
     Kelsparse = sp.sparse.csr_matrix(Kel)
-    nelem     = con_el.shape[0]                # number of elements
 
-    for ii in range(nelem) :
-        ix   = con_el[ii]
-        Rind = c_[dofpn*ix, dofpn*ix+1].flatten()
-        Cind = arange(dofpe)
-        indV = ones(dofpe)
-        Emat = sp.sparse.csr_matrix((indV, (Rind,Cind)), shape=(ndof,dofpe))
-        K    = K + Emat.dot(Kelsparse).dot(Emat.T)
+    ix   = con_el
+    Rind = c_[dofpn*ix, dofpn*ix+1].flatten()
+    Cind = arange(dofpe)
+    indV = ones(dofpe)
+    Emat = sp.sparse.csr_matrix((indV, (Rind,Cind)), shape=(ndof,dofpe))
+    K    = K + Emat.dot(Kelsparse).dot(Emat.T)
+
+
+    # dofpe     = Kel.shape[0]
+    # npe       = con_el.shape[1]     # nodes per element
+    # dofpn     = dofpe/npe           # dof per node
+    # ndof      = K.shape[0]
+    # Kelsparse = sp.sparse.csr_matrix(Kel)
+    # nelem     = con_el.shape[0]                # number of elements
+
+    # for ii in range(nelem) :
+    #     ix   = con_el[ii]
+    #     Rind = c_[dofpn*ix, dofpn*ix+1].flatten()
+    #     Cind = arange(dofpe)
+    #     indV = ones(dofpe)
+    #     Emat = sp.sparse.csr_matrix((indV, (Rind,Cind)), shape=(ndof,dofpe))
+    #     K    = K + Emat.dot(Kelsparse).dot(Emat.T)
 
     return K
 
